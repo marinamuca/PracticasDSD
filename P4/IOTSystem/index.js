024b6fb8510feb6664ddde0b804b3e5070fc19fe
Marina Muñoz <marinamuca01@gmail.com>
2022-05-22 12:40:11 +0200

    Db server

--- P4/IOTSystem/index.js
+++ P4/IOTSystem/index.js
@@ -1,4 +1,8 @@
 const HttpServer = require("./httpServer.js");
+const DBServer = require("./dbServer.js");
 
 var httpServer = new HttpServer();
+var dbServer = new DBServer("mongodb://localhost:27017/","DSDP4");
+
 httpServer.launch();
+dbServer.launch();
