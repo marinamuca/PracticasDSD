a2ba15f7913fdaab31b11eeea45c7f7bb0378844
Marina Muñoz <marinamuca01@gmail.com>
2022-05-23 12:13:35 +0200

    add gitignore

--- P4/IOTSystem/index.js
+++
@@ -1,8 +0,0 @@
-const HttpServer = require("./httpServer.js");
-const mongoDBServer = require("./mongodbServer.js");
-
-var httpServer = new HttpServer();
-var mongoDBServer = new DBServer("mongodb://localhost:27017/","DSDP4");
-
-httpServer.launch();
-mongoDBServer.launch();
